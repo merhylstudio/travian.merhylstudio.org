1ad0f44a3554a1ac6187afdc031f698012688f93
Maghin <maghin@devcentos7.merhylstudio.org>
2016-09-17 19:17:26 +0200

    add composer.json and images 	modified:   Capfile 	new file:   composer.json 	modified:   config/deploy.rb 	new file:   web/view/img/merhylstudio.png 	new file:   web/view/img/merhylstudio_small.png 	new file:   web/view/img/shark.ico 	new file:   web/view/img/shark.png

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ lock '3.6.1'
 set :log_level, :debug
 
 # Remote server and website common parameters
-set :application, "travian.merhylstudio.org"
+set :application, "travian"
 set :deploy_user, "billy"
 set :deploy_to, "/app/httpd/www/#{fetch(:application)}"
 set :keep_releases, 5
