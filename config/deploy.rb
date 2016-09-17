c140967589be53d96103e7a664254311add211a1
Maghin <maghin@devcentos7.merhylstudio.org>
2016-09-17 22:20:11 +0200

    modified:   config/deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,7 +12,7 @@ set :keep_releases, 5
 
 # Git parameters with URL to repository (can be local)
 set :scm, :git
-set :repo_url, 'https://git@github.com/merhylstudio/travian.git'
+set :repo_url, 'https://git@github.com/merhylstudio/travian.merhylstudio.org.git'
 #ask :branch, 'git rev-parse --abbrev-ref HEAD'.chomp
 set :branch, 'master'
 
