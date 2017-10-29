34bfc004cd5db71a1d99e19eed742e790f3f750e
Maghin <realmaghin@hotmail.com>
2017-10-29 02:28:04 +0200

    fix table name

--- app/web/inactives.php
+++ app/web/inactives.php
@@ -136,7 +136,7 @@ if (isset($_POST["submit"]))
   include_once './controler/classLoader.php';
   echo '<div class="container-fluid bg-lightblue">'.PHP_EOL;
 
-  $ret = Inactive::get($world.'travian'.$country, $sx, $sy, $sdl, $sdh, $spl, $sph);
+  $ret = Inactive::get($world.'_travian_'.$country, $sx, $sy, $sdl, $sdh, $spl, $sph);
 //  var_dump($ret);
   # Affichage
   echo '<div class="col-xs-10 col-xs-offset-1">'.PHP_EOL;
